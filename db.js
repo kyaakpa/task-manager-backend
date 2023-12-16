41c228e73b831891d5441a6374296d10dcc327ea
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2023-12-16 22:59:40 +1100

    working POST GET PUT DELETE methods

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: "postgres",
   password: process.env.DB_PASSWORD,
   host: "localhost",
-  port: 4200,
+  port: 5432,
   database: "taskmanager",
 });
 
--- index.js
+++ index.js
@@ -10,18 +10,65 @@ app.use(express.json());
 //create a task
 app.post("/tasks", async (req, res) => {
   try {
-    console.log(req.body);
+    const { description } = req.body;
+    const newTask = await pool.query(
+      "INSERT INTO tasks (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTask.rows);
   } catch (err) {
     console.error(err.message);
   }
 });
 //get all tasks
-
+app.get("/tasks", async (req, res) => {
+  try {
+    const allTasks = await pool.query("SELECT * FROM tasks");
+    res.json(allTasks.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 //get a task
-
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getSingleTask = await pool.query(
+      "SELECT * FROM tasks WHERE task_id= $1",
+      [id]
+    );
+    res.json(getSingleTask.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 //update a task
-
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    console.log(req.body);
+    const updateTask = await pool.query(
+      "UPDATE tasks SET description=$1 WHERE task_id=$2",
+      [description, id]
+    );
+    res.json("Task was updated.");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 //delete a task
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTask = await pool.query("DELETE FROM tasks WHERE task_id=$1", [
+      id,
+    ]);
+    res.json("Task was deleted.");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5500, () => {
   console.log("Server started on port 5500.");
